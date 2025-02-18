a05c5989b74cee6fe94a2d5d68613fdfd440cbf0
MP Yeager <mpy@mpy.dev>
2025-02-18 22:30:43 +0000

    fix: Simplify UmbraCore imports
    - Remove incorrect module imports
    - Keep only core dependencies

--- Sources/UmbraCore/UmbraCore.swift
+++ Sources/UmbraCore/UmbraCore.swift
@@ -1,8 +1,5 @@
 @_exported import CryptoTypes
-@_exported import CryptoTypes_Types
-@_exported import CryptoTypes_Protocols
 @_exported import SecurityTypes
-@_exported import UmbraLogging
 
 /// Main entry point for UmbraCore functionality
 public enum UmbraCore {
@@ -18,14 +15,3 @@ public enum UmbraCore {
 // Re-export main types and protocols
 public typealias SecurityProvider = SecurityTypes.SecurityProvider
 public typealias SecurityError = SecurityTypes.SecurityError
-
-// Re-export logging types
-public typealias LogEntry = UmbraLogging.LogEntry
-public typealias LogLevel = UmbraLogging.LogLevel
-public typealias LoggingError = UmbraLogging.LoggingError
-public typealias LoggingProtocol = UmbraLogging.LoggingProtocol
-public typealias LoggingService = UmbraLogging.LoggingService
-
-// Re-export crypto types
-public typealias CryptoError = CryptoTypes_Types.CryptoError
-public typealias CryptoService = CryptoTypes_Protocols.CryptoService
