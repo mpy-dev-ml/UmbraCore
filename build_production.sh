01335b8430636749bcd926a26ea4289745392328
Matthew Yeager <mpy@wookiee.co.uk>
2025-03-13 00:01:31 +0000

    Delete build_production.sh

--- build_production.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Build all production targets from production_targets.txt
-bazelisk build $(cat production_targets.txt | grep -v "^//" | sed 's/^/\/\//' | tr '\n' ' ') --config=prod "$@"
